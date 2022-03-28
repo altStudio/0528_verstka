f698a1b47860aea2fc3e7a3ecc1ef09cbfc448ad
yeashy <gertyn07@gmail.com>
2022-03-28 18:19:03 +0300

    modal + sending mail

--- popup.js
+++ popup.js
@@ -1,8 +1,8 @@
 
-const openModal = (triggerOpen,modal,triggerClose) => {
-    let openBtn = document.querySelectorAll('.'+triggerOpen),
-        modalContent = document.querySelector('.'+modal),
-        closeBtn = document.querySelectorAll('.'+modal + ' ' + '.'+triggerClose);
+const openModal = (triggerOpen, modal, triggerClose) => {
+    let openBtn = document.querySelectorAll('.' + triggerOpen),
+        modalContent = document.querySelector('.' + modal),
+        closeBtn = document.querySelectorAll('.' + modal + ' ' + '.' + triggerClose);
 
     openBtn.forEach(item => {
         item.addEventListener('click', function (e) {
@@ -25,3 +25,20 @@ openModal('site-menu', 'menu-modal', 'close-btn-trigger');
 openModal('subscribe-open', 'subscribe-modal', 'close-btn');
 openModal('policy-btn-for-modal', 'privacy-policy', 'close-btn');
 openModal('cookie-btn-for-modal', 'cookie-policy', 'close-btn');
+
+document.getElementById('email-form-2').onsubmit = (e) => {
+    e.preventDefault();
+    fetch('./sendMail.php', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            email: $('#email-input').val(),
+            name: $('#name-input').val()
+        })
+    })
+    .then(data => {
+        console.log(data);
+    })
+}
