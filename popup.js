a9db030a7ff59fea34ffc2955091e7ebb8d205cd
yeashy <gertyn07@gmail.com>
2022-03-28 20:18:52 +0300

    catch errors from response

--- popup.js
+++ popup.js
@@ -15,7 +15,8 @@ const openModal = (triggerOpen, modal, triggerClose) => {
         item.addEventListener('click', function (e) {
             e.preventDefault();
             modalContent.classList.remove('modal-show');
-
+            $('.modal-success-block').css('z-index', '0');
+            $('.modal-success-block').css('opacity', '0');
         });
     })
 
@@ -26,9 +27,9 @@ openModal('subscribe-open', 'subscribe-modal', 'close-btn');
 openModal('policy-btn-for-modal', 'privacy-policy', 'close-btn');
 openModal('cookie-btn-for-modal', 'cookie-policy', 'close-btn');
 
-document.getElementById('email-form-2').onsubmit = (e) => {
-    e.preventDefault();
-    fetch('./sendMail.php', {
+
+function getAnswer() {
+    return fetch('./sendMail.php', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -38,7 +39,46 @@ document.getElementById('email-form-2').onsubmit = (e) => {
             name: $('#name-input').val()
         })
     })
-    .then(data => {
-        console.log(data);
-    })
+        .then(response => {
+            if (!response.ok) {
+                if (response.status === 400) {
+                    return response.json();
+                } else {
+                    return {
+                        message: '500'
+                    };
+                }
+            }
+            else {
+                return response.json();
+            }
+        })
+}
+
+document.getElementById('email-form-2').onsubmit = (e) => {
+    e.preventDefault();
+    let answer = getAnswer()
+        .then(response => useResponseForAction(response.message))
+}
+
+let interval;
+
+function useResponseForAction(message) {
+    $('.modal-error').css('color', 'black');
+    if (message === '500') {
+        //something went wrong
+    } else if (message != 'OK'){
+        message.forEach((element, index) => {
+            $($('.modal-error')[index]).css('color', '#FA1B43');
+        });
+    } 
+    else {
+        let c = 0;
+        $('.modal-success-block').css('z-index', '1');
+        interval = setInterval(() => {
+            c += 0.01
+            $('.modal-success-block').css('opacity', `${c}`);
+            if (c > 1) clearInterval(interval);
+        }, 10);
+    }
 }
